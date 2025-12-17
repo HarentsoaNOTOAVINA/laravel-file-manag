7cec81e32592ca4c2830a697ec08009fdbebaa46
Harentsoa NOTOAVINA <mae7ryi@gmail.com>
2025-12-17 14:48:46 +0300

    feat: Implement core file management features including file listing, sharing, and starring, along with dependency updates and UI adjustments.

---
+++ resources/js/pages/files/ShareFiles.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import AppLayout from '@/layouts/AppLayout.vue';
+import type { BreadcrumbItem } from '@/types';
+import { dashboard, share_files } from '@/routes';
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: dashboard().url
+    },
+    {
+        title: 'Files',
+        href: share_files().url
+    }
+];
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div
+            class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border p-4"
+        >
+            <h1>Share file with me</h1>
+        </div>
+    </AppLayout>
+</template>
+
+<style scoped>
+
+</style>
