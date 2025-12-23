e5d671e0cd727df9de449fad451ee7cc1e899b39
Harentsoa NOTOAVINA <mae7ryi@gmail.com>
2025-12-23 11:59:23 +0300

    feat: Implement hierarchical file listing, create user root directories, and add supporting services, resources, and tests.

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use App\Traits\HasCreatorAndUpdater;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Kalnoy\Nestedset\NodeTrait;
 
+/**
+ * @method static where(string $string, int|string|null $id)
+ */
 class File extends Model
 {
-    //
+    use HasFactory, HasCreatorAndUpdater, NodeTrait, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'path',
+        'is_folder',
+        'mime',
+        'size',
+        'parent_id',
+        'created_by',
+        'updated_by',
+    ];
 }
